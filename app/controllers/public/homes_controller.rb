cbff0154b64ffc06bc6ac1fd60fded252eb01fc7
koske0125 <kosuke.0125.dm@gmail.com>
2022-05-21 20:40:49 +0000

    [Update]顧客トップページに実際の商品とジャンルが反映されるように

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
+    @Genres = Genre.all
+    @items = Item.all
+    @items = Item.all.order(created_at: :desc).limit(4)
   end
 
   def about
