05a5daa6a4da6c8b218eac550c8d188fcfadf72c
koske0125 <kosuke.0125.dm@gmail.com>
2022-05-21 06:09:35 +0000

    [Add]顧客情報編集機能の実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -6,11 +6,16 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
-
+    @customer = Customer.find(current_customer.id)
   end
 
   def update
-
+    @customer = Customer.find(current_customer.id)
+    if @customer.update(customer_params)
+      redirect_to "/customers/my_page", notice: "You have updated Profile successfully."
+    else
+      render "edit"
+    end
   end
 
   def unsubscribe
@@ -19,6 +24,12 @@ class Public::CustomersController < ApplicationController
 
   def withdraw
 
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:email,:postal_code,:address,:telephone_number)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,42 @@
 Rails.application.routes.draw do
 
-  devise_for :customers, controllers: {
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-  devise_for :admin, controllers: {
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
-  namespace :public, path: "" do
+ namespace :admin do
     get '/' => 'homes#top'
-    get '/about' => 'homes#about'
-    get '/my_page' => 'customers#show'
+    patch '/order_details/:id' => 'order_details#update'
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :orders, only: [:show, :update]
+  end
+
+# get '/customers/is_deleted' => 'public/customers#is_deleted'
+namespace :public, path: "" do
+  get '/customers/my_page' => 'customers#show'
+  get '/customers/is_deleted' => 'customers#is_deleted'
+  patch '/customers/withdraw' => 'customers#withdraw'
+  get '/' => 'homes#top'
+  get '/about' => 'homes#about'
+  post '/orders/confirm' => 'orders#confirm'
+  get '/orders/complete' => 'orders#complete'
+  delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+  resources :customers, only: [:edit, :update]
+  resources :items, only: [:index, :show]
+  resources :send_addresses, only: [:index, :edit, :create, :update, :destroy]
+  resources :orders, only: [:new, :create, :index, :show]
+  resources :cart_items, only: [:index, :update, :destroy, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
